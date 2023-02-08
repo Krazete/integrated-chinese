493d249483d1510855096e6c46db4fcd60f8c4c6
Ted Michael Celis <krazete@gmail.com>
2023-02-07 22:23:15 -0800

    tab change

--- data/index.js
+++ data/index.js
@@ -2,23 +2,23 @@
 exercises: ['word', 'sentence', 'paragraph']
 ---
 var data = {
-{%- for exercise in page.exercises %}
+    {%- for exercise in page.exercises %}
     "{{ exercise }}": {
-    {%- for level in site.data[exercise] %}
+        {%- for level in site.data[exercise] %}
         "{{ level[0] }}": [
-        {%- for lesson in level[1] %}
+            {%- for lesson in level[1] %}
             {{- lesson[0] }}
             {%- unless forloop.last %}, {% endunless %}
-        {%- endfor -%}
+            {%- endfor -%}
         ]
         {%- unless forloop.last %},{% endunless %}
-    {%- endfor %}
+        {%- endfor %}
     },
-{%- endfor %}
+    {%- endfor %}
     "title": {
-    {%- for lesson in site.data.index %}
+        {%- for lesson in site.data.index %}
         "{{ lesson[0] }}": "{{ lesson[1] }}"
         {%- unless forloop.last %},{% endunless %}
-    {%- endfor %}
+        {%- endfor %}
     }
 };
