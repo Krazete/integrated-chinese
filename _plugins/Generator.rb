f923180ac5f8eb199345fe5022e84454ba616242
Ted Michael Celis <krazete@gmail.com>
2025-02-07 01:27:24 -0800

    reorganized a bit
    mostly the generator

--- _plugins/Generator.rb
+++ _plugins/Generator.rb
@@ -1,46 +1,51 @@
 module MainPlugin
-    class PageGenerator < Jekyll::Generator
-        def generate(site)
-            # Review Pages
-            site.data['review'].each do |review|
-                site.pages << WordPage.new(site, review, 'review')
+    module ZHint
+        # convert integers to chinese numerals (0-99 only)
+        def zhint(n)
+            if n == 0
+                return '零'
             end
-            # Exercise Pages
-            # exercises = ['word', 'sentence', 'paragraph']
-            # data = site.data['index']
-            # data.each do |datum|
-            #     site.pages << WordPage.new(site, datum)
+            digit = ['', '一', '二', '三', '四', '五', '六', '七', '八', '九']
+            number = ''
+            if n >= 20
+                number += digit[(n - (n % 10)) / 10]
+            end
+            if n >= 10
+                number += '十'
+            end
+            return number + digit[n % 10]
+        end
+    end
+    class ReviewPage < Jekyll::Page
+        include ZHint
+        def initialize(site, base, dir, basename, title)
+            @site = site
+            @base = base
+            @dir = dir
+            @basename = basename
+            @ext = '.html'
+            @name = basename + '.html'
+            @data = {
+                'layout' => dir,
+                'title' => "Lesson #{basename} Review",
+                'heading-zh' => "第#{zhint(basename.to_i)}課",
+                'heading-en' => title,
+                'data' => "review/#{basename}.js",
+                'scripts' => ['mp3.js', 'review.js']
+            }
+        end
+    end
+    class PageGenerator < Jekyll::Generator
+        safe true
+        def generate(site)
+            # Review
+            site.data['index'].each do |index|
+                site.pages << ReviewPage.new(site, site.source, 'review', index[0], index[1])
+            end
+            # Exercise (Word)
+            # site.data['index'].each do |index|
+            #     site.pages << ReviewPage.new(site, datum)
             # end
         end
     end
-    class WordPage < Jekyll::Page
-        def initialize(site, datum, pagetype)
-            @site = site
-            @base = site.source
-            @dir = pagetype
-            @basename = datum[0]
-            @ext = '.html'
-            @name = @basename + @ext
-            @data = {
-                'layout' => pagetype,
-                'title-en' => datum[0],
-                'title-zh' => '第' + number_zh(datum[0].to_i) + '課'
-            }
-    end
-    def number_zh(n)
-        if n == 0
-            return '零'
-        end
-        digit = ['', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十']
-        digit_zh = ''
-        if n >= 20
-            digit_zh += digit[(n - (n % 10)) / 10]
-        end
-        if n >= 10
-            digit_zh += '十'
-        end
-        digit_zh += digit[n % 10]
-        return digit_zh
-    end
-end
 end
